78cdbae2264fc729969d1ff4bcfc9041e2147729
Timchenko Bogdan <tbd2504@.gmail.com>
2023-03-14 18:23:10 +0200

    Add test for user_activate

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -1,4 +1,4 @@
-from accounts.forms import UserRegisterForm, UserUpdateForm
+from accounts.forms import UserRegisterForm, UserUpdateForm, UserReactivationForm
 from accounts.models import User
 
 from django.core.signing import Signer
@@ -15,30 +15,30 @@ class TestUserRegisterView(TestCase):
             'email': 'user_1@test.com',
         }
         self.client = Client()
-        self.register_url = reverse('accounts:register')
-        self.register_done_url = reverse('accounts:register_done')
+        self.registration_url = reverse('accounts:register')
+        self.registration_done_url = reverse('accounts:register_done')
 
     def test_contain_template(self):
-        response = self.client.get(self.register_url)
+        response = self.client.get(self.registration_url)
         self.assertTemplateUsed(response, 'accounts/user_register.html')
 
     def test_contain_form(self):
-        response = self.client.get(self.register_url)
+        response = self.client.get(self.registration_url)
         form = response.context.get('form')
         self.assertIsInstance(form, UserRegisterForm)
 
     def test_success_redirect(self):
-        response = self.client.post(self.register_url, self.data)
-        self.assertRedirects(response, self.register_done_url, status_code=302, target_status_code=200)
-        self.assertEqual(response.url, self.register_done_url)
+        response = self.client.post(self.registration_url, self.data)
+        self.assertRedirects(response, self.registration_done_url, status_code=302, target_status_code=200)
+        self.assertEqual(response.url, self.registration_done_url)
 
     def test_not_success_redirect(self):
         self.data['password1'] = '123qwe!@'
-        response = self.client.post(self.register_url, self.data)
+        response = self.client.post(self.registration_url, self.data)
         self.assertNotEqual(302, response.status_code)
 
     def test_registration_valid(self):
-        self.client.post(self.register_url, self.data)
+        self.client.post(self.registration_url, self.data)
 
         user = User.objects.first()
 
@@ -51,7 +51,7 @@ class TestUserRegisterView(TestCase):
     def test_register_invalid(self):
         self.data['password1'] = '123qwe!@'
 
-        response = self.client.post(self.register_url, self.data)
+        response = self.client.post(self.registration_url, self.data)
         form = response.context['form']
         user = User.objects.filter(username=self.data['username'])
 
@@ -135,3 +135,66 @@ class TestUserProfileUpdateView(TestCase):
         self.not_required_data.update(self.required_data)
         response = self.client.post(self.profile_update_url, self.not_required_data)
         self.assertNotEqual(302, response.status_code)
+
+
+class TestUserReactivationView(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            'username': 'user',
+            'password': '123qwe!@#',
+            'email': 'user@test.com',
+        }
+        User.objects.create_user(**self.data)
+        self.client = Client()
+        self.reactivation_url = reverse('accounts:reactivation')
+        self.reactivation_done_url = reverse('accounts:reactivation_done')
+
+    def test_contain_template(self):
+        response = self.client.get(self.reactivation_url)
+        self.assertTemplateUsed(response, 'accounts/user_reactivation.html')
+
+    def test_contain_form(self):
+        response = self.client.get(self.reactivation_url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, UserReactivationForm)
+
+    def test_success_redirect(self):
+        response = self.client.post(self.reactivation_url, {'email': self.data['email']})
+        self.assertRedirects(response, self.reactivation_done_url, status_code=302, target_status_code=200)
+        self.assertEqual(response.url, self.reactivation_done_url)
+
+    def test_not_success_redirect(self):
+        self.data['email'] = 'not_exist_user@test.com'
+        response = self.client.post(self.reactivation_url, {'email': self.data['email']})
+        self.assertNotEqual(302, response.status_code)
+
+
+class TestUserActivate(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            'username': 'user_1',
+            'password1': '123qwe!@#',
+            'password2': '123qwe!@#',
+            'email': 'user_1@test.com',
+        }
+        self.client = Client()
+        self.registration_url = reverse('accounts:register')
+        self.registration_done_url = reverse('accounts:register_done')
+
+    def test_activation_url(self):
+        response = self.client.post(self.registration_url, self.data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.registration_done_url, status_code=302, target_status_code=200)
+
+        user = User.objects.first()
+        self.assertEqual(self.data['username'], user.username)
+
+        signer = Signer()
+        response = self.client.get(
+            'http://localhost' + reverse('accounts:register_activate', kwargs={'sign': signer.sign(user.username)})
+        )
+        self.assertEqual(200, response.status_code)
+
+        user.refresh_from_db()
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_activated)
