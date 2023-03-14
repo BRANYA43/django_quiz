74c646e7d584e36c095ee1d6bd1482d9610c9d9d
Timchenko Bogdan <tbd2504@.gmail.com>
2023-03-14 12:04:26 +0200

    Add validators test

--- src/accounts/tests/test_validators.py
+++ src/accounts/tests/test_validators.py
@@ -1,2 +1,24 @@
-class TestValidators:
-    pass
+from unittest import TestCase
+
+from django.core.exceptions import ValidationError
+
+from accounts.models import User
+from accounts.validators import validate_email_exist
+
+
+class TestValidators(TestCase):
+    email = 'user@test.com'
+
+    def setUp(self):
+        User.objects.create(
+            username='user',
+            password='123qwe!@#',
+            email=self.email
+        )
+
+    def test_validate_email_exist(self):
+        # exist email
+        self.assertIsNone(validate_email_exist(self.email))
+
+        # not exist email
+        self.assertRaises(ValidationError, validate_email_exist, self.email + '123')
