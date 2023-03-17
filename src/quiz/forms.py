b44831ad2767e7a478b03d5e49eb1cdc80de12f5
Timchenko Bogdan <tbd2504@.gmail.com>
2023-03-17 15:32:51 +0200

    Add validate order num

--- src/quiz/forms.py
+++ src/quiz/forms.py
@@ -6,13 +6,29 @@ from .models import Choice
 
 class QuestionInlineFormSet(forms.BaseInlineFormSet):
     def clean(self):
-        forms_ = [form for form in self.forms if not form.cleaned_data['DELETE']]
-        if not (self.instance.QUESTION_MIN_LIMIT <= len(forms_) <= self.instance.QUESTION_MAX_LIMIT):
+        if not (self.instance.QUESTION_MIN_LIMIT <= len(self.forms) <= self.instance.QUESTION_MAX_LIMIT):
             raise ValidationError(
-                f'Questions count must be range '
+                'Questions count must be range '
                 f'from {self.instance.QUESTION_MIN_LIMIT} '
                 f'to {self.instance.QUESTION_MAX_LIMIT} inclusive'
             )
+        order_nums = [form.instance.order_num for form in self.forms]
+
+        for i, order_num in enumerate(order_nums):
+
+            if not (1 <= order_num <= 100):
+                raise ValidationError(
+                    'Order num must be range from 1 ' 
+                    f'to {self.instance.QUESTION_MAX_LIMIT} inclusive'
+                )
+            if i == 0 and order_num != 1:
+                raise ValidationError(
+                    'First order num must start 1.'
+                )
+            elif i != 0 and order_num - order_nums[i-1] != 1:
+                raise ValidationError(
+                    'Order num must increment 1 from past order num.'
+                )
 
 
 class ChoiceInlineFormSet(forms.BaseInlineFormSet):
